9b1fc752587fcaf4383b48cd853382dc20076bdd
gongyong <794668717@qq.com>
2022-03-27 22:45:24 +0800

    more funtions and toydataset tests

--- grain_46_26_6_104_dnnbyhand.py
+++ grain_46_26_6_104_dnnbyhand.py
@@ -53,52 +53,47 @@ class Grain_Dataset(Dataset):
     def __len__(self):
         return self.total_num
 
-class DNN(nn.Module):
-    def __init__(self, layers):
-        super(DNN, self).__init__()
-        
-        # parameters
-        self.depth = len(layers) - 1
-        
-        # set up layer order dict
-        self.activation = torch.nn.Tanh
-        
-        layer_list = list()
-        for i in range(self.depth - 1): 
-            layer_list.append(
-                ('layer_%d' % i, torch.nn.Linear(layers[i], layers[i+1]))
-            )
-            layer_list.append(('activation_%d' % i, self.activation()))
-            
-        layer_list.append(
-            ('layer_%d' % (self.depth - 1), torch.nn.Linear(layers[-2], layers[-1]))
-        )
-        layerDict = OrderedDict(layer_list)
-        
-        # deploy layers
-        self.layers = torch.nn.Sequential(layerDict)
-        
-    def forward(self, x):
-        out = self.layers(x)
-        return out
-
-
 # the physics-guided neural network
 class PhysicsInformedNN():
-    def __init__(self,device):
+    def __init__(self,device):        
+        self.iter = 0
 
-        layers = [4, 100, 200, 200, 100, 1]
-        # deep neural networks
-        self.dnn = DNN(layers).to(device)
-        
+        self.layers = [4, 100, 200, 200, 100, 1]
+        self.weights, self.biases = self.initialize_NN(self.layers, device)
+        self.parameter_dict  = [{"params":self.weights}, {"params":self.biases}]
+
         # optimizers: using the same settings
         #self.optimizer_SGD = torch.optim.SGD(self.dnn.parameters(),lr = 1e-3)        
-        self.optimizer_Adam = torch.optim.Adam(self.dnn.parameters(),lr = 1e-3, betas = (0.9,0.999),eps = 1e-8)
+        self.optimizer_Adam = torch.optim.Adam(self.parameter_dict,lr = 1e-3, betas = (0.9,0.999),eps = 1e-8)
         #self.scheculer = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer_Adam, mode='min', factor=0.5, patience=5)
-        self.iter = 0
-    
+
+    def initialize_NN(self, layers, device):        
+        weights = []
+        biases = []
+        num_layers = len(layers) 
+        for l in range(0,num_layers-1):
+            W = torch.zeros([layers[l], layers[l+1]]).to(device).requires_grad_(True)
+            nn.init.xavier_normal_(W)
+            b = torch.zeros([1,layers[l+1]]).to(device).requires_grad_(True)
+            weights.append(W)
+            biases.append(b)   
+
+        return weights, biases
+
+    def dnn(self, X):
+        num_layers = len(self.weights) + 1
+
+        for l in range(0,num_layers-2):
+            W = self.weights[l]
+            b = self.biases[l]
+            X = torch.tanh(torch.add(torch.matmul(X, W), b))
+        W = self.weights[-1]
+        b = self.biases[-1]
+        Y = torch.add(torch.matmul(X, W), b)
+
+        return Y
         
-    def net_h(self, x,y,z,t): 
+    def net_h(self, x, y, z, t): 
         X = torch.cat([x,y,z,t], dim=1) 
         h = self.dnn(X)
         return h 
@@ -120,7 +115,6 @@ class PhysicsInformedNN():
         return f
 
     def train_Adam(self,x, y, z, t, h):
-        self.dnn.train()
 
         h_pred = self.net_h(x, y, z, t)
         f_pred = self.net_f(x, y, z, t)
@@ -171,12 +165,6 @@ if __name__ == '__main__':
     train_loader = DataLoader(dataset=train_set, batch_size = 16384 ,shuffle=True,pin_memory=False)
     
     model = PhysicsInformedNN(device)
-
-    for m in model.dnn.modules():
-        if isinstance(m,torch.nn.Linear):
-            nn.init.xavier_normal_(m.weight)
-            nn.init.constant_(m.bias, 0.0)
- 
 
     #train
     for epoch in range(epochs_num):
